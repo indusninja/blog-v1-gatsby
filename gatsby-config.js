3f5e44126afabcf96716542fcf12240be4beaafe
Prakash Prasad <prakashnos@gmail.com>
2022-03-03 21:55:06 +0100

    fix for remark embedder

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,17 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-embedder`,
+            options: {
+              customTransformers: [
+                // Your custom transformers
+              ],
+              services: {
+                // The service-specific options by the name of the service
+              },
+            },
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 800,
